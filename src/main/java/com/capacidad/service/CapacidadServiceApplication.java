fd11eaf81b84c70231672cb03ceec0e821375362
Nicolas Santillan <nicolas.santillan@pragma.com.co>
2024-09-30 09:13:14 -0500

    Configurando dependencias y application properties

---
+++ src/main/java/com/capacidad/service/CapacidadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.capacidad.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacidadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacidadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capacidad/service/CapacidadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capacidad.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapacidadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
